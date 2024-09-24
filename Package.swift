c58471183ee57d49380f8442583080e806f32fa4
Anka <runner@Mac-1727185197884.local>
2024-09-24 13:48:03 +0000

    Releasing version 3.7.27 - Xcode 15.0 Dynamic

--- Package.swift
+++ Package.swift
@@ -11,7 +11,6 @@ let package = Package(
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(
 			name: "QuantumIOS",
-			type: .dynamic,
 			targets: ["QuantumIOS", "MuppetIOS"])
 	],
 	targets: [
